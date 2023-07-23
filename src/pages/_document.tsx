4b03610db4e9a2c05606ec3b015a893f659a8cbd
Emil Widlund <hello@emilwidlund.com>
2023-07-23 20:53:36 +0200

    Push temp site

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className="bg-[url('/bg.jpg')] bg-cover bg-center">
         <Main />
         <NextScript />
       </body>

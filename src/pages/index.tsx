7d25029108684efc0c6af14edc42e1840ae30cc4
Emil Widlund <hello@emilwidlund.com>
2023-07-24 23:11:49 +0200

    Fix responsive

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,9 +19,9 @@ export default function Home() {
     >
       <canvas id='gradient-canvas' className='fixed top-0 right-0 bottom-0 left-0 w-full h-full' />
       <div className='relative flex flex-col justify-center items-center p-24 min-h-full z-10'>
-        <Image className='absolute top-24' alt="Logotype" src="/logo.svg" width={160} height={100}></Image>
-        <h1 className='text-6xl max-w-3xl text-center leading-normal'>At the intersection of design & technology</h1>
-        <span className='absolute bottom-24 text-xl'>Coming in 2023</span>
+        <Image className='absolute top-12 lg:top-24' alt="Logotype" src="/logo.svg" width={140} height={100}></Image>
+        <h1 className='text-5xl lg:text-6xl max-w-3xl text-center leading-normal lg:leading-normal'>At the intersection of design & technology</h1>
+        <span className='absolute bottom-12 lg:bottom-24 text-xl'>Coming in 2023</span>
       </div>
     </main>
   )

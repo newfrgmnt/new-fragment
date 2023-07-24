defe8c935d01321946fc26511aec973e901d9939
Emil Widlund <hello@emilwidlund.com>
2023-07-24 23:05:26 +0200

    Replace bg

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="bg-[url('/bg.jpg')] bg-cover bg-center">
+      <body>
         <Main />
         <NextScript />
       </body>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,28 @@
 import { Inter } from 'next/font/google'
 import Image from 'next/image';
+import { useEffect } from 'react';
+import {Gradient} from '../components/MeshGradient/MeshGradient';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+   useEffect(() => {
+        const gradient = new Gradient();
+
+        // @ts-ignore
+        gradient.initGradient('#gradient-canvas');
+    }, []);
+
   return (
     <main
-      className={`relative flex min-h-screen flex-col justify-center items-center ${inter.className}`}
+      className={`flex flex-col h-screen ${inter.className}`}
     >
-      <Image alt="Logotype" src="/logo.svg" width={200} height={100}></Image>
-      <span className='absolute bottom-16'>Coming in 2023</span>
+      <canvas id='gradient-canvas' className='fixed top-0 right-0 bottom-0 left-0 w-full h-full' />
+      <div className='relative flex flex-col justify-center items-center p-24 min-h-full z-10'>
+        <Image className='absolute top-24' alt="Logotype" src="/logo.svg" width={160} height={100}></Image>
+        <h1 className='text-6xl max-w-3xl text-center leading-normal'>At the intersection of design & technology</h1>
+        <span className='absolute bottom-24 text-xl'>Coming in 2023</span>
+      </div>
     </main>
   )
 }

87c4d57ee31165013347e8baa72e4e8effd56a72
Emil Widlund <hello@emilwidlund.com>
2023-09-22 10:36:39 +0200

    Temporary redirect

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: 'https://github.com/newfrgmnt',
+        permanent: false,
+        basePath: false
+      },
+    ]
+  },
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
